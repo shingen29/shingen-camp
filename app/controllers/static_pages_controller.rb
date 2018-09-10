c2d4bd0e6f20456301e158db26798586b9a8658b
Makoto Otsuka <mametra2580@gmail.com>
2018-09-10 12:12:28 +0900

    add checkout method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,14 @@ class StaticPagesController < ApplicationController
   end
   def stripe
   end
+  def checkout
+    require "stripe"
+    Stripe.api_key = "sk_test_DC07L0Ysgzk4UpqNUuUNZYPB"
+    Stripe::Charge.create(
+      amount: 500,
+      currency: 'jpy',
+      customer: "cus_DZpTcyGFxvYTRe",
+      description: "メモ"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'stripe' => 'static_pages#stripe'
+  post 'checkout' => 'static_pages#checkout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
